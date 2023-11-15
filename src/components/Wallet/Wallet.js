9a51310bd8531ec7407c897d0d44c380d7155434
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-11-15 19:34:58 +0400

    add tabs

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -1,15 +1,33 @@
 import './style.css'
 import { SiWalletconnect } from "react-icons/si";
+import Tabs from "./Tab";
 
 export const AppWallet= () => {
+    const tabs = [
+        {
+            label: 'Deposit',
+            content: <div className='tab-content'>Select the token to deposit (Min 0.0001BTC)</div>,
+        },
+        {
+            label: 'Withdraw',
+            content: <div>Content for Tab 2</div>,
+        },
+        {
+            label: 'Buy crypto',
+            content: <div>Content for Tab 3</div>,
+        },
+    ];
     return (
         <div className="header-wallet">
             <button className="rounded-button">
                 <div className='wallet-icon'>
                     <SiWalletconnect size='20'/> WalletConnect
                 </div>
-
             </button>
+
+            <div>
+                <Tabs tabs={tabs} />
+            </div>
         </div>
     )
 }
