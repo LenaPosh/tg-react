b78e6b5ccd85a35e52a822b28efad49d1ac39da5
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-11-17 12:59:26 +0400

    add qr-code

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import { SiWalletconnect } from 'react-icons/si';
 import Tabs from './Tab';
 import { DropdownWallet } from './Dropdown';
 import CopyableAddress from './PaymentButton';
+import QRCode from 'qrcode.react';
 
 export const AppWallet = () => {
-    const paymentAddress = 'Your payment address is here';
+    const paymentAddress = '0x97d03eF9Ffe1Ac3Cb2ADa6D20C71d139245bdd7b';
+    const [qrCodeValue, setQRCodeValue] = useState('');
+
+    useEffect(() => {
+        setQRCodeValue(paymentAddress);
+    }, [paymentAddress]);
 
     const tabs = [
         {
@@ -16,6 +22,10 @@ export const AppWallet = () => {
                     Select the token to deposit (Min 0.0001BTC)
                     <DropdownWallet />
                     <CopyableAddress address={paymentAddress} />
+                    <div className="qr-code-container">
+                        <QRCode value={qrCodeValue} className="qr-code" />
+                        <p className="qr-code-text">Send only BTC to this deposit address. <br/> Values sent bellow the minimum amount or to an incorrect address will be lost.</p>
+                    </div>
                 </div>
             ),
         },
@@ -24,7 +34,7 @@ export const AppWallet = () => {
             content: <div>Content for Tab 2</div>,
         },
         {
-            label: 'Buy crypto',
+            label: 'Buy Crypto',
             content: <div>Content for Tab 3</div>,
         },
     ];
