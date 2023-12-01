6b9853e8b07e5d0661bc5e29eb11feae780e8c53
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-12-01 19:44:52 +0400

    change CandlestickChart2

--- src/pages/TradePage.js
+++ src/pages/TradePage.js
@@ -3,6 +3,8 @@ import {AppFooter} from "../components/Footer/Footer";
 // import MyGraph from "../components/Trade/Trade";
 // import MyGraphSecond from "../components/Trade/example";
 import CandlestickChart from "../components/Trade/apexcharts";
+import CandlestickChart2 from "../components/Trade/Realapex";
+import './style.css'
 
 
 export const TradePage = () => {
@@ -12,6 +14,10 @@ export const TradePage = () => {
             {/*<MyGraph/>*/}
             {/*<MyGraphSecond/>*/}
             <CandlestickChart/>
+            <div className='CandlestickChart2'>
+                <CandlestickChart2/>
+            </div>
+
             <AppFooter/>
         </>
 
